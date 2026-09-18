4c91e02d797bcdface04defbd982c93fcadbb5b4
Matar X <omaralisalama8@gmail.com>
2026-09-18 22:44:04 +0300

    update files

--- backend/test.js
+++ backend/test.js
@@ -745,4 +745,9 @@ app.post("/checkout", async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`ZELL server is running on port ${PORT}`);
+});
+
 module.exports = app;
