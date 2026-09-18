5d35fec2e502bf9ba25ef552f1794ca462ba307d
Matar-X <omaralisalama8@gmail.com>
2026-09-18 20:49:18 +0300

    Add server listening on specified port

--- backend/test.js
+++ backend/test.js
@@ -745,4 +745,9 @@ app.post("/checkout", async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
