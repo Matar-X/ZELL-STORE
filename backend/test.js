d5e30a19907822f7935e8ca7ff7565e314766399
Matar X <omaralisalama8@gmail.com>
2026-09-19 13:33:20 +0300

    fix: remove transporter verify from codebase

--- backend/test.js
+++ backend/test.js
@@ -233,24 +233,6 @@ function isBirthdayToday(birthdate) {
 const { Resend } = require("resend");
 
 
-// اختبار الاتصال عند تشغيل السيرفر
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("❌ Email Transporter Connection Failed:", error.message);
-    } else {
-        console.log("✅ Email Transporter is ready to send messages");
-    }
-});
-
-// اختبار الاتصال عند تشغيل السيرفر
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("❌ Email Transporter Connection Failed:", error.message);
-    } else {
-        console.log("✅ Email Transporter is ready to send messages");
-    }
-});
-
 const ADMIN_EMAILS = [
     "omaralisalama8@gmail.com",
     "nadamoamed73@gmail.com",
